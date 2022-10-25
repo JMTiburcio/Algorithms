ff721e5fbbc50cf78a63b4cf9636ae933babf40b
JMTiburcio <jmrtme@gmail.com>
2022-10-25 10:48:19 -0300

    adding other solutions for capSentence problem

--- capSentence.js
+++ capSentence.js
@@ -4,11 +4,27 @@ return the equivalent of the sentence when capitalised. E.g
   capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
 */
 
+//Using map and slice
+
 function capSentence(text) {
-    return text
-            .toLowerCase()
-            .split(' ')
-            .map(word => word[0].toUpperCase() + word.slice(1))
-            .join(' ')
+  const words = text.toLowerCase().split(' ');
+  const capText = words.map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+  return capText
 }
- 
+
+//Using forEach and slice
+
+function capSentence(text) {
+  const words = text.toLowerCase().split(' ');
+  const capText = [];
+  words.forEach(word => capText.push(word[0].toUpperCase() + word.slice(1)))
+  return capText.join(' ')
+}
+
+//Using map and replace
+
+function capSentence(text) {
+  const words = text.toLowerCase().split(' ');
+  const capText = words.map(word => word.replace(word[0], word[0].toUpperCase())).join(' ');
+  return capText
+}
