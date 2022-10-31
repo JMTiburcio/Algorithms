604ec7d365f60e34378abf18eef61c3a63b65e3f
JMTiburcio <jmrtme@gmail.com>
2022-10-31 11:22:44 -0300

    factorial algorithm using recursion

---
+++ factorial.js
@@ -0,0 +1,14 @@
+/*
+Write a function that returns the factorial of 
+the provided integer(n). E.g
+    factorial(5) // should return 120
+*/
+
+
+function factorial(n) {
+    if(n === 0) { return 1 }
+    
+    return n * factorial(n - 1)
+}
+
+console.log(factorial(5))
